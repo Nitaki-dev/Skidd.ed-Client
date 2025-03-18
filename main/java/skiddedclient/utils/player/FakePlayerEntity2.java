c58e2ddd0f1d0560e1896342348156ec1e118860
nitaki-dev <64524466+Nitaki-dev@users.noreply.github.com>
2025-03-18 22:18:01 +0100

    Freecam without mixins :flushed:
    sexy

---
+++ main/java/skiddedclient/utils/player/FakePlayerEntity2.java
@@ -0,0 +1,29 @@
+package skiddedclient.utils.player;
+
+import java.util.UUID;
+
+import com.mojang.authlib.GameProfile;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.Identifier;
+
+public class FakePlayerEntity2 extends AbstractClientPlayerEntity {
+
+    public FakePlayerEntity2(ClientWorld world, GameProfile gameProfile) {
+        super(world, gameProfile, null);
+    }
+
+    public void setUUID(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public Identifier getSkinTexture() {
+        if (hasSkinTexture())
+            return super.getSkinTexture();
+        else
+            return MCAPIHelper.INSTANCE.getPlayerSkin(MinecraftClient.getInstance().getSession().getProfile() == null ? this.uuid : MinecraftClient.getInstance().getSession().getProfile().getId());
+    }
+}
